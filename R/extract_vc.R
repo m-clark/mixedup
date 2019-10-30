8f12d4f691a7dcad3980280455ae4c2a3438522a
Michael Clark <micl@umich.edu>
2019-10-30 08:20:48 -0400

    alter approach to extract_vc to better deal with correlation

--- R/extract_vc.R
+++ R/extract_vc.R
@@ -5,56 +5,46 @@ extract_vc <- function(model) {
   UseMethod('extract_random_coef')
 }
 
-extract_vc.merMod <- function(model, show_cor = FALSE, digits = 3) {
+extract_vc.merMod <- function(model, ci = .95, show_cor = FALSE, digits = 3) {
   vc_mat = VarCorr(model)
 
+  # if (ci > 0) # add check on level because...
+  #   ci = confint(model) # I'm not sure what thought went into the rownames but this is almost useless output
+  vc = data.frame(vc_mat)
+  colnames(vc) = c('group', 'coefficient', 'coefficient_2', 'variance', 'sd')
+
+  # cleanup/add
+  vc = dplyr::filter(vc, is.na(coefficient) | is.na(coefficient_2))
+  vc = dplyr::mutate(vc,
+                     proportion = variance / sum(variance),
+                     coefficient = gsub(coefficient, pattern = '[\\(,\\)]', replacement = ''),
+                     coefficient = ifelse(is.na(coefficient), '', coefficient))
+  vc = dplyr::select(vc, -coefficient_2)
+  vc = dplyr::mutate_if(vc, is.numeric, round, digits = digits)
 
   if (show_cor) {
-    vc = data.frame(VarCorr(lmer_4))
-    colnames(vc) = c('group', 'variable_1', 'variable_2')
-    vc
+    cormats = lapply(vc_mat, attr, 'correlation')
+
+    remove_parens = function(x) {
+      colnames(x) = gsub(colnames(x), pattern = '[\\(,\\)]', replacement = '')
+      rownames(x) = colnames(x)
+      x
+    }
+
+    cormats <- lapply(cormats, remove_parens)
+    cormats <- lapply(cormats, round, digits = digits)
+
+    return(list(`Variance Components` = vc, Cor = cormats))
   }
 
-  # remove covariances/correlations
-  vc = formatVC(vc_mat)
-  vc = data.frame(vc, stringsAsFactors = FALSE)
-  vc$Name = gsub(vc$Name, pattern = '[\\(,\\)]', replacement = '')
-  vc$Std.Dev. <- as.numeric(vc$Std.Dev.)
-  vc$Corr <- as.numeric(vc$Corr)
-  vc$variance <- vc$Std.Dev.^2
-  vc$proportion <- vc$variance / sum(vc$variance)
-
-  rownames(vc) = NULL
-
-  vc = mutate(vc, Groups = ifelse(Groups == '', NA, Groups))
-  vc = tidyr::fill(vc, Groups)
-
-  vc = dplyr::rename(vc, sd = Std.Dev.)
-  vc = dplyr::rename_all(vc, tolower)
-  vc = dplyr::rename_at(vc,
-                        vars(matches('^v[0-9]')),
-                        function(x) gsub(x, pattern =  '^v', replacement =  'corr'))
-  vc = dplyr::rename_at(vc,
-                        vars(matches('^corr')),
-                        function(x) paste0('cor_', vc$name[1:length(x)]))
-  vc = dplyr::mutate(vc,
-                     name = gsub(
-                       name,
-                       pattern = '[\\(,\\)]',
-                       replacement = ''
-                       )
-                     )
-
-
-
-  dplyr::mutate_if(vc, is.numeric, round, digits = digits)
+  vc
 }
 
-
+library(lme4)
 lmer_2 <- lmer(Reaction ~ Days + (1 + Days | Subject), data = sleepstudy)
 debugonce(extract_vc.merMod)
 extract_vc.merMod(lmer_2)
-extract_vc.merMod(lmer_2, pretty = T)
+extract_vc.merMod(lmer_2, show_cor = T)
 
 
 # nonsensically complex model
